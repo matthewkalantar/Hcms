57cc2f1e0a73f45a27e1b7db0dc6bbe40e0f487a
matthewkalantar <matthewkalantar@gmail.com>
2023-08-16 23:13:54 +0200

    Update MongoDbRepository

--- src/Core/AgileCMS.Data/DataSettingsManager.cs
+++ src/Core/AgileCMS.Data/DataSettingsManager.cs
@@ -5,35 +5,40 @@ namespace AgileCMS.Domain.Data
 {
 
     /// <summary>
-    /// Manager of data settings (connection string)
+    /// Manage connection string (Load from /ِDatabaseSettings.cfg)
+    /// We Won't store connection str to appsettings.json because of
+    /// we need to writable file that created aumaticly during installation (Settings.cfg)
+    /// we need one instanse of this class in whole the application so make it static
     /// </summary>
     public static class DataSettingsManager
     {
         private static DataSettings _dataSettings;
 
+        private static bool? _databaseIsInstalled;
+
+
         /// <summary>
-        /// Load settings
+        /// Load settings from DatabaseSettings.cfg
         /// </summary>
-        public static DataSettings LoadSettings(bool reloadSettings = false)
+        public static DataSettings LoadSettings()
         {
-            //if (!reloadSettings && _dataSettings != null)
-            //    return _dataSettings;
+            if ( _dataSettings != null)
+                return _dataSettings;
 
-            //if (!File.Exists(CommonPath.SettingsPath))
-            //    return new DataSettings();
+            // TODO: Move Path and file name to a static constant
+            string settingFilePath = "/App_Data/ِDatabaseSettings.cfg";
+            if (!File.Exists(settingFilePath)) return new DataSettings();
 
-            //try
-            //{
-            //    var text = File.ReadAllText(CommonPath.SettingsPath);
-            //    _dataSettings = JsonSerializer.Deserialize<DataSettings>(text);
-            //}
-            //catch
-            //{
-            //    //Try to read file
-            //    var connectionString = File.ReadLines(CommonPath.SettingsPath).FirstOrDefault();
-            //    _dataSettings = new DataSettings() { ConnectionString = connectionString, DbProvider = DbProvider.MongoDB };
-
-            //}
+            try
+            {
+                var text = File.ReadAllText(settingFilePath);
+                //Map settings
+                _dataSettings = JsonSerializer.Deserialize<DataSettings>(text);
+            }
+            catch (Exception ex)
+            {
+                // TODO: Create default connection string on exception
+            }
             return _dataSettings;
         }
     }
--- src/Core/AgileCMS.Data/MongoDbRepository.cs
+++ src/Core/AgileCMS.Data/MongoDbRepository.cs
@@ -1,5 +1,6 @@
 ﻿using AgileCMS.Domain;
 using AgileCMS.Domain.Data;
+using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,116 +12,230 @@ namespace AgileCMS.Data
 {
     public class MongoDbRepository<T> : IRepository<T> where T : BaseEntity
     {
-        public IQueryable<T> Table => throw new NotImplementedException();
+        #region Fields
+        protected IMongoCollection<T> _mongoCollection;
 
-        public Task ClearAsync()
+        /// <summary>
+        /// Gets the collection
+        /// </summary>
+        public IMongoCollection<T> MongoCollection
+        {
+            get
+            {
+                return _mongoCollection;
+            }
+        }
+
+        /// <summary>
+        /// MongoDb Database
+        /// </summary>
+        protected IMongoDatabase _database;
+        public IMongoDatabase Database
+        {
+            get
+            {
+                return _database;
+            }
+        }
+        #endregion
+
+        #region Ctor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>        
+        public MongoDbRepository()
+        {
+            var connection = DataSettingsManager.LoadSettings();
+
+            if (!string.IsNullOrEmpty(connection.ConnectionString))
+            {
+                var client = new MongoClient(connection.ConnectionString);
+                var DbNameName = new MongoUrl(connection.ConnectionString).DatabaseName;
+                _database = client.GetDatabase(DbNameName);
+                _mongoCollection = _database.GetCollection<T>(typeof(T).Name);
+            }
+
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Clear entities
+        /// </summary>
+        public virtual Task ClearAsync()
+        {
+            return _mongoCollection.DeleteManyAsync(Builders<T>.Filter.Empty);
+        }
+
+        /// <summary>
+        /// Delete entity
+        /// </summary>
+        /// <param name="entity">Entity</param>
+        public virtual void Delete(T entity)
+        {
+            _mongoCollection.FindOneAndDelete(e => e.Id == entity.Id);
+        }
+
+        /// <summary>
+        /// Delete entities (Bulk)
+        /// </summary>
+        /// <param name="entities">Entities</param>
+        public virtual void Delete(IEnumerable<T> entities)
+        {
+            foreach (T entity in entities)
+            {
+                _mongoCollection.FindOneAndDeleteAsync(e => e.Id == entity.Id);
+            }
+        }
+
+        /// <summary>
+        /// Async Delete entity
+        /// </summary>
+        /// <param name="entity">Entity</param>
+        public virtual async Task<T> DeleteAsync(T entity)
+        {
+            await _mongoCollection.DeleteOneAsync(e => e.Id == entity.Id);
+            return entity;
+        }
+
+        /// <summary>
+        /// Async Delete entities (Bulk)
+        /// </summary>
+        /// <param name="entities">Entities</param>
+        public virtual async Task<IEnumerable<T>> DeleteAsync(IEnumerable<T> entities)
+        {
+            foreach (T entity in entities)
+            {
+                await DeleteAsync(entity);
+            }
+            return entities;
+        }
+
+        /// <summary>
+        /// Async Delete many entities
+        /// </summary>
+        /// <param name="filterexpression"></param>
+        /// <returns></returns>
+        public virtual async Task DeleteManyAsync(Expression<Func<T, bool>> filterexpression)
+        {
+            await _mongoCollection.DeleteManyAsync(filterexpression);
+        }
+
+        /// <summary>
+        /// Get all entities in collection
+        /// </summary>
+        /// <returns>collection of entities</returns>
+        public virtual Task<List<T>> GetAllAsync()
+        {
+            return  _mongoCollection.AsQueryable().ToListAsync();
+        }
+
+
+        /// <summary>
+        /// Get entity by Id
+        /// </summary>
+        /// <param name="id">Identifier</param>
+        /// <returns>Entity</returns>
+        public virtual T GetById(string id)
+        {
+            return _mongoCollection.Find(e => e.Id == id).FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Get Async entity by Id
+        /// </summary>
+        /// <param name="id">Identifier</param>
+        /// <returns>Entity</returns>
+        public virtual Task<T> GetByIdAsync(string id)
+        {
+            return _mongoCollection.Find(e => e.Id == id).FirstOrDefaultAsync();
+        }
+
+        /// <summary>
+        /// Insert entity
+        /// </summary>
+        /// <param name="entity">Entity</param>
+        public virtual T Insert(T entity)
+        {
+            _mongoCollection.InsertOne(entity);
+            return entity;
+        }
+
+        /// <summary>
+        /// Async Insert entity
+        /// </summary>
+        /// <param name="entity">Entity</param>
+        public virtual async Task<T> InsertAsync(T entity)
+        {
+            await _mongoCollection.InsertOneAsync(entity);
+            return entity;
+        }
+
+        /// <summary>
+        /// Insert entities
+        /// </summary>
+        /// <param name="entities">Entities</param>
+        public virtual void Insert(IEnumerable<T> entities)
+        {
+            _mongoCollection.InsertMany(entities);
+        }
+
+        /// <summary>
+        /// Async Insert entities
+        /// </summary>
+        /// <param name="entities">Entities</param>
+        public virtual async Task<IEnumerable<T>> InsertAsync(IEnumerable<T> entities)
+        {
+            await _mongoCollection.InsertManyAsync(entities);
+            return entities;
+        }
+
+        public virtual Task Pull(string id, Expression<Func<T, IEnumerable<string>>> field, string element)
         {
             throw new NotImplementedException();
         }
 
-        public void Delete(T entity)
+        public virtual Task PullFilter<U, Z>(string id, Expression<Func<T, IEnumerable<U>>> field, Expression<Func<U, Z>> elemFieldMatch, Z elemMatch)
         {
             throw new NotImplementedException();
         }
 
-        public void Delete(IEnumerable<T> entities)
+        public virtual Task PullFilter<U>(string id, Expression<Func<T, IEnumerable<U>>> field, Expression<Func<U, bool>> elemFieldMatch)
         {
             throw new NotImplementedException();
         }
 
-        public Task<T> DeleteAsync(T entity)
+        public virtual IQueryable<T> TableCollection(string collectionName)
         {
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<T>> DeleteAsync(IEnumerable<T> entities)
+        public virtual T Update(T entity)
         {
             throw new NotImplementedException();
         }
 
-        public Task DeleteManyAsync(Expression<Func<T, bool>> filterexpression)
+        public virtual void Update(IEnumerable<T> entities)
         {
             throw new NotImplementedException();
         }
 
-        public Task<List<T>> GetAllAsync()
+        public virtual Task<T> UpdateAsync(T entity)
         {
             throw new NotImplementedException();
         }
 
-        public T GetById(string id)
+        public virtual Task<IEnumerable<T>> UpdateAsync(IEnumerable<T> entities)
         {
             throw new NotImplementedException();
         }
 
-        public Task<T> GetByIdAsync(string id)
+        public virtual Task UpdateField<U>(string id, Expression<Func<T, U>> expression, U value)
         {
             throw new NotImplementedException();
         }
-
-        public T Insert(T entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Insert(IEnumerable<T> entities)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<T> InsertAsync(T entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<T>> InsertAsync(IEnumerable<T> entities)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task Pull(string id, Expression<Func<T, IEnumerable<string>>> field, string element)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task PullFilter<U, Z>(string id, Expression<Func<T, IEnumerable<U>>> field, Expression<Func<U, Z>> elemFieldMatch, Z elemMatch)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task PullFilter<U>(string id, Expression<Func<T, IEnumerable<U>>> field, Expression<Func<U, bool>> elemFieldMatch)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IQueryable<T> TableCollection(string collectionName)
-        {
-            throw new NotImplementedException();
-        }
-
-        public T Update(T entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Update(IEnumerable<T> entities)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<T> UpdateAsync(T entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<T>> UpdateAsync(IEnumerable<T> entities)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task UpdateField<U>(string id, Expression<Func<T, U>> expression, U value)
-        {
-            throw new NotImplementedException();
-        }
+        public virtual IQueryable<T> Table => _mongoCollection.AsQueryable();
+        #endregion
     }
 }
